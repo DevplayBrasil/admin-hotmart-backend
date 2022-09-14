0fb0b378f5f6c92101b5887aaeea0b28d80e1d3f
Jhonatan <jhonatanjuniocp@gmail.com>
2022-09-13 21:00:56 -0300

    Atualizando a pasta build

--- build/start/routes.js
+++ build/start/routes.js
@@ -7,4 +7,7 @@ const Route_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Route
 Route_1.default.get('/', async () => {
     return { hello: 'world' };
 });
+Route_1.default.get('/healthz', () => {
+    return { result: 'all good!' };
+});
 //# sourceMappingURL=routes.js.map
